0977402d5d9cb8742aa74d78b829dedc144c46a9
Jayanthkumar04 <jayanthkumar598@gmail.com>
2025-02-07 14:51:03 +0530

    retreving all products and next is adding a product

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IProduct } from './Models/IProduct';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllProducts()
+  {
+   return this.http.get<IProduct[]>(`https://localhost:7154/product/getAll`);
+  }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../Models/IProduct';
+import { HttpClient } from '@angular/common/http';
+import { ProductService } from '../product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+
+  products!:IProduct[];
+
+  formState=false;
+
+  constructor(private http:ProductService)
+  {
+       this.displayAllProducts();
+  }
+
+  ngOnInit(): void {
+    
+        this.displayAllProducts();
+
+  }
+
+  displayAllProducts()
+  {
+       this.http.getAllProducts().subscribe({
+        next:(data)=>{
+          this.products = data;
+        }
+       })
+  }
+
+  toogleState()
+  {
+    this.formState = !this.formState;
+  }
+
+  formData()
+  {
+    console.log("formdata")
+    this.formState = false;
+  }
+
+
+
 
 }
